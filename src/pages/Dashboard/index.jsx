872988a9e86e2386342b05f9a3ea2c06a8cdd967
beatrizmarioti-ieg <b.tmarioti3.0@gmail.com>
2025-10-30 11:14:10 -0300

    feat: adicionando dashboard a pagina

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,14 @@
+import styles from "./styles.module.css";
+
 export default function Dashboard() {
   return (
-    <div style={{ padding: 24 }}>
-      <h1>Dash</h1>
+    <div className={styles.container}>
+      <iframe
+        title="BI-Expotech"
+        src="https://app.powerbi.com/view?r=eyJrIjoiNGU0ZTA0MzEtYWQwZS00MDcyLTk4NTItOWM5Y2M2NzVhMGU4IiwidCI6ImIxNDhmMTRjLTIzOTctNDAyYy1hYjZhLTFiNDcxMTE3N2FjMCJ9"
+        className={styles.iframe}
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }
