05379387af826c167ca259877b077aa494599841
beatrizmarioti-ieg <b.tmarioti3.0@gmail.com>
2025-11-02 09:57:48 -0300

    fix: colocando mais um dashboard na pagina de dashboards

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -3,12 +3,23 @@ import styles from "./styles.module.css";
 export default function Dashboard() {
   return (
     <div className={styles.container}>
-      <iframe
-        title="BI-Expotech"
-        src="https://app.powerbi.com/view?r=eyJrIjoiNGU0ZTA0MzEtYWQwZS00MDcyLTk4NTItOWM5Y2M2NzVhMGU4IiwidCI6ImIxNDhmMTRjLTIzOTctNDAyYy1hYjZhLTFiNDcxMTE3N2FjMCJ9"
-        className={styles.iframe}
-        allowFullScreen
-      ></iframe>
+      <div className={styles.dashboard}>
+        <iframe
+          title="BI-Expotech"
+          src="https://app.powerbi.com/view?r=eyJrIjoiNGU0ZTA0MzEtYWQwZS00MDcyLTk4NTItOWM5Y2M2NzVhMGU4IiwidCI6ImIxNDhmMTRjLTIzOTctNDAyYy1hYjZhLTFiNDcxMTE3N2FjMCJ9"
+          className={styles.iframe}
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      <div className={styles.dashboard}>
+        <iframe
+          title="Nutria"
+          src="https://app.powerbi.com/view?r=eyJrIjoiZGYyZWI3MmMtOTBhYS00ZjFjLWJiYTYtNGIwY2RmZTMyZmE5IiwidCI6ImIxNDhmMTRjLTIzOTctNDAyYy1hYjZhLTFiNDcxMTE3N2FjMCJ9"
+          className={styles.iframe}
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
